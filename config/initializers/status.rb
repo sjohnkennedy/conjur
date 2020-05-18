ff695dc4435b060b31811257d5e309b26b79e2a4
Moti Cohen <moti.cohen@cyberark.com>
2020-05-18 18:47:11 +0300

    On startup, adding printout of "Conjur Version: XXXX", Based on file 'VERSION':
      API key for admin: 1pc4ves3bba8091vn2fvt11454za1k7f1z41a44g7a1m3yg0r1tks6fr
      Creating socket: audit_test_1_47298905114040.sock
      Conjur Version: 1.6.0
      /src/conjur-server/app/domain/authentication/authn_azure/authenticator.rb:7: warning: already initialized constant Authentication::AuthnAzure::Log
      /src/conjur-server/app/domain/authentication/authn_azure/application_identity.rb:4: warning: previous definition of Log was here

--- config/initializers/status.rb
+++ config/initializers/status.rb
@@ -1,3 +1,5 @@
 # frozen_string_literal: true
 
 ENV["CONJUR_VERSION_DISPLAY"] = File.read(File.expand_path("../../VERSION", File.dirname(__FILE__)))
+
+puts "Conjur Version: #{ENV["CONJUR_VERSION_DISPLAY"]}"
