d8439fb63964e5f83f19bdbe5fe830ef1e17dd32
Hadar Artzi <hadar.artzi@gmail.com>
2020-05-18 14:18:19 -0400

    add unit tests

--- spec/util_spec.rb
+++ spec/util_spec.rb
@@ -4,6 +4,8 @@ require 'spec_helper'
 
 require 'util/struct'
 
+require 'util/error_code'
+
 describe Util::Struct do
   subject(:struct) { Class.new(Util::Struct) { field :req, opt: 42 } }
 
@@ -39,5 +41,33 @@ describe Util::Struct do
       child = Class.new(struct) { to_s "hello world" }
       expect { child.new.inspect }.not_to raise_error
     end
+  end
+end
+
+describe Util::ErrorCode do
+  let(:error_code_valid) { Util::ErrorCode.new('./app/domain/errors.rb') }
+  let(:error_code_invalid) { Util::ErrorCode.new('invalid_file_path') }
+  let(:error_code_empty) { Util::ErrorCode.new('./spec/models/errors/empty') }
+  
+  it 'raises error when file path is invalid' do
+    expect { error_code_invalid }.to raise_error(FileNotFound)
+  end
+
+  it 'outputs the next available error code' do 
+    expect do
+      error_code_valid.next_available
+    end.to output("The next available error number is 58 ( CONJ00058E )\n").to_stdout
+  end
+
+  xit 'outputs next available error code but forgets to append leading zeros' do 
+    expect do
+      error_code_valid.next_available
+    end.to output("The next available error number is 58 ( CONJ58E )\n").to_stdout
+  end
+
+  it 'outputs that the file lacks error codes' do 
+    expect do
+      error_code_empty.next_available
+    end.to output("The file doesn't contain any error messages\n").to_stdout
   end
 end
